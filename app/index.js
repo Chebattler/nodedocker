6e8f97b80a29594117b0c7edd917a9adaf9b5388
Chebattler <Chebattler@mail.ru>
2022-06-05 02:32:20 +0200

    add mongo

--- app/index.js
+++ app/index.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const morgan = require('morgan');
 
+
 const app = express();
 app.use(morgan('combined'));
+
+
+
+//DB MODULE
+var dbo = null // global variable to hold the connection
+const MongoClient = require('mongodb').MongoClient;
+
+
+const dbName = '234';
+const client = new MongoClient('mongodb://root:shantanubansal@20.126.215.234:27017/234/?authSource=admin&readPreference=primaryPreferred&appname=MongoDB');
+
+// Connect to MongoDB server, run the findDocuments function and close the connection.
+client.connect(function(err) {
+
+    if(err) console.log(err);
+    console.log('Connected successfully to MongoDB server on port 27017');
+    dbo = client.db(dbName);
+
+
+});
+
+
 
 
 app.get('/', (req, res) => {
